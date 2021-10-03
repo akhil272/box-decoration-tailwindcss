201f4c01b85f955f3430c73bf61975561d1dca5b
akhil272 <akhilms272@gmail.com>
2021-10-03 19:20:15 +0530

    box decoration utilities tailwindcss completed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 function Homepage() {
   return (
-    <h1 className="text-center my-24 font-black tracking-tight text-6xl">
-      Home
-    </h1>
+    <div className="min-h-screen bg-indigo-900">
+      <div className="max-w-6xl p-24">
+        <h1 className="text-5xl font-semibold text-white leading-snug bg-gradient-to-r from-blue-500 to-transparent inline px-12 decoration-clone hover:decoration-slice">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco
+        </h1>
+      </div>
+    </div>
   );
 }
 
